4dffca355e1564f25f2a9f6a0d007aa05312339a
Kaoru Sato <kaorunix@gmail.com>
2012-08-04 14:35:52 +0900

    controller作成

--- app/jp/scala/controllers/Application.scala
+++ app/jp/scala/controllers/Application.scala
@@ -2,9 +2,20 @@ package jp.scala.controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+
+case class UserForm(login:String, name:String, email:String, sex:Int)
 
 object Application extends Controller {
-
+  val userForm = Form(
+    mapping(
+      "login" -> nonEmptyText,
+      "name" -> nonEmptyText,
+      "email" -> email,
+      "sex" -> number
+    )(UserForm.apply)(UserForm.unapply)
+  )
   def index = TODO
   def create = TODO
 }
